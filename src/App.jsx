df9f7973cb8bac7752c8d905c94f407e34464d77
adenugbamichael <adenugbamicke@gmail.com>
2023-04-11 15:28:21 +0100

    connecting header | creating OAuth credentials

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import StreamShow from "./components/streams/StreamShow"
 import StreamEdit from "./components/streams/StreamEdit"
 import StreamCreate from "./components/streams/StreamCreate"
 import StreamDelete from "./components/streams/StreamDelete"
+import Header from "./components/Header"
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' exact element={<StreamList />} />
-        <Route path='/streams/show' exact element={<StreamShow />} />
-        <Route path='/streams/edit' exact element={<StreamEdit />} />
-        <Route path='/streams/new' exact element={<StreamCreate />} />
-        <Route path='/streams/delete' exact element={<StreamDelete />} />
-      </Routes>
-    </BrowserRouter>
+    <div className='ui container'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' exact element={<StreamList />} />
+          <Route path='/streams/show' exact element={<StreamShow />} />
+          <Route path='/streams/edit' exact element={<StreamEdit />} />
+          <Route path='/streams/new' exact element={<StreamCreate />} />
+          <Route path='/streams/delete' exact element={<StreamDelete />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
